547cc0548731410ddccec4f80fc14c51c914b471
George Anderson <george@benevolentcode.com>
2019-01-06 17:01:09 -0500

    remove duplicate names

--- install.sh
+++ install.sh
@@ -20,17 +20,6 @@ if ! sudo grep -q "%wheel		ALL=(ALL) NOPASSWD: ALL #george/dotfiles" "/etc/sudoe
 
   # Keep-alive: update existing sudo time stamp until the script has finished
   while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
-
-  bot "Do you want me to setup this machine to allow you to run sudo without a password?\nPlease read here to see what I am doing:\nhttp://wiki.summercode.com/sudo_without_a_password_in_mac_os_x \n"
-
-  read -r -p "Make sudo passwordless? [y|N] " response
-
-  if [[ $response =~ (yes|y|Y) ]];then
-      sudo cp /etc/sudoers /etc/sudoers.back
-      echo '%wheel		ALL=(ALL) NOPASSWD: ALL #george/dotfiles' | sudo tee -a /etc/sudoers > /dev/null
-      sudo dscl . append /Groups/wheel GroupMembership $(whoami)
-      bot "You can now run sudo commands without password!"
-  fi
 fi
 
 # /etc/hosts
