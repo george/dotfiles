e606ea8b0c95b97f8adf6098474e09fc76816560
George Anderson <george@benevolentcode.com>
2019-01-06 17:37:13 -0500

    and more

--- install.sh
+++ install.sh
@@ -1039,7 +1039,8 @@ defaults write com.apple.messageshelper.MessageController SOInputLineSettings -d
 bot "OK. Note that some of these changes require a logout/restart to take effect. Killing affected applications (so they can reboot)...."
 for app in "Activity Monitor" "Address Book" "Calendar" "Contacts" "cfprefsd" \
   "Dock" "Finder" "Mail" "Messages" "Safari" "SizeUp" "SystemUIServer" \
-  "iCal" "Terminal"; do
+  # "iCal" "Terminal"; do
+  "iCal"; do
   killall "${app}" > /dev/null 2>&1
 done
 
